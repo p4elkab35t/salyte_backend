e55ff72e0393aa83f0a88c43db6d7bc749907ed2
p4elkab35t <saganiter234@gmail.com>
2025-03-11 22:01:30 -0700

    random fix +cors

--- gateway/src/misc/request.ts
+++ gateway/src/misc/request.ts
@@ -1,7 +1,7 @@
 export class corsResponse extends Response {
     constructor(body?: any, init?: any) {
       super(body, init);
-      this.headers.set("Access-Control-Allow-Origin", "*/*");
+      this.headers.set("Access-Control-Allow-Origin", "*");
       this.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
       this.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, authorization");
     }
