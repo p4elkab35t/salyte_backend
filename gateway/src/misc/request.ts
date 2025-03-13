e85a54985ada8e5846d986126debcac288201d35
p4elkab35t <saganiter234@gmail.com>
2025-03-13 05:18:13 -0700

    remove cahcecontrol header

--- gateway/src/misc/request.ts
+++ gateway/src/misc/request.ts
@@ -4,6 +4,6 @@ export class corsResponse extends Response {
       this.headers.set("Access-Control-Allow-Origin", "*");
       this.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
       this.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, authorization, Cache-Control");
-      this.headers.set("Cache-Control", "private, max-age=60");
+      // this.headers.set("Cache-Control", "private, max-age=60");
     }
   }
